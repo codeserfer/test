1203ed203eac8e4068e210f8ba39f0ebf6322000
codeserfer <admin@codeserfer.com>
2014-02-05 22:32:01 +0400

    first commit

---
+++ test/main.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+void swap (int &a, int &b) {
+    int c=a;
+    a=b;
+    b=c;
+}
+
+int main () {
+    int a=5, b=6;
+    swap (a, b);
+    cout << "a="<<a<<" b="<<b<<endl;
+
+    return 0;
+}
