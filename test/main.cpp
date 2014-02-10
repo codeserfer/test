7c2288454547fde61ea83f3d98fc4583ffc1ea78
codeserfer <admin@codeserfer.com>
2014-02-10 23:07:18 +0400

    second commit

--- test/main.cpp
+++ test/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-void swap (int &a, int &b) {
+/*void swap (int &a, int &b) {
     int c=a;
     a=b;
     b=c;
@@ -13,5 +13,56 @@ int main () {
     swap (a, b);
     cout << "a="<<a<<" b="<<b<<endl;
 
+    return 0;
+}
+*/
+
+class Product {
+public:
+    virtual string getName()=0;
+    virtual ~Product(){}
+};
+
+class ConcreteProductA: public Product {
+public:
+    string getName() { return "ConcreteProductA"; }
+};
+
+class ConcreteProductB : public Product {
+public:
+    string getName() { return "ConcreteProductB"; }
+};
+
+class Creator {
+public:
+    virtual Product* factoryMethod() = 0;
+};
+
+class ConcreteCreatorA: public Creator {
+public:
+    Product* factoryMethod() {
+        return new ConcreteProductA();
+    }
+};
+
+class ConcreteCreatorB : public Creator {
+public:
+    Product* factoryMethod() {
+        return new ConcreteProductB();
+    }
+};
+
+int main() {
+    static const size_t count = 2;
+    ConcreteCreatorA CreatorA;
+    ConcreteCreatorB CreatorB;
+    // An array of creators
+    Creator* creators[count] = {&CreatorA,&CreatorB};
+    // Iterate over creators and create products
+    for (size_t i = 0; i < count; i++) {
+        Product* product = creators[i]->factoryMethod();
+        cout << product->getName() << endl;
+        delete product;
+    }
     return 0;
 }
